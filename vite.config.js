d2d4943201e71a1c771403b7b2bd11c4beaba0b6
ashish verma <ashishverma042020@gmail.com>
2025-06-27 18:50:53 +0530

    vite fix 2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     outDir: 'dist',
     assetsInclude: ['**/*.jsx'], // Ensure JSX files are treated as assets
   },
-});
+})
