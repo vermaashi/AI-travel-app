8c671a9a34a3e25a7c3eee46ae74404aa123bc6d
ashish verma <ashishverma042020@gmail.com>
2025-06-27 18:29:44 +0530

    footer fix

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -6,44 +6,44 @@ import { toast } from 'sonner';
 import InfoSection from '../components/InfoSection';
 import Hotels from '../components/Hotels';
 import PlacesToVisit from '../components/PlacesToVisit';
-import Footer from '../components/footer';
-// import footer from '../components/footer';
+import Footer from '../components/Footer';
 
 function Viewtrip () {
-    const {tripId} = useParams();
-    const [trip,setTrip]=useState([]);
-    useEffect(()=>{
-        tripId&&GetTripData();
-    },[tripId])
+    const { tripId } = useParams();
+    const [trip, setTrip] = useState({}); // FIX: initialize as object
 
-    // use to get trip informatio from firebase
-    const GetTripData = async() => {
-        const docRef = doc(db,'AITrips', tripId);
-        const docsnap=await getDoc(docRef);
-
-        if (docsnap.exists()){
-            console.log("Document: ",docsnap.data());
-            setTrip(docsnap.data());
+    useEffect(() => {
+        if (tripId) {
+            GetTripData();
         }
-        else
-        console.log("NO such Documnet found.");
-        toast('no tirp found!')
-        
-    }
-  return (
-    <div className='p-10 md:px-20 lg:px-44 xl:px-56'>
+        // eslint-disable-next-line
+    }, [tripId]);
 
-        { /* {Information section} */  }
-                <InfoSection trip= {trip} />
-        { /* {Hotel recommandation} */}
-                <Hotels trip= {trip}/>
-        { /* {Daily plan} */ }
-                <PlacesToVisit trip= {trip} />
-        { /* {footer} */ }
-                <Footer trip= {trip} />
-            
-    </div>
-    
-  )
+    // use to get trip information from firebase
+    const GetTripData = async () => {
+        const docRef = doc(db, 'AITrips', tripId);
+        const docsnap = await getDoc(docRef);
+
+        if (docsnap.exists()) {
+            console.log("Document: ", docsnap.data());
+            setTrip(docsnap.data());
+        } else {
+            console.log("NO such Document found.");
+            toast('no trip found!'); // FIX: typo
+        }
+    }
+
+    return (
+        <div className='p-10 md:px-20 lg:px-44 xl:px-56'>
+            {/* {Information section} */}
+            <InfoSection trip={trip} />
+            {/* {Hotel recommendation} */}
+            <Hotels trip={trip} />
+            {/* {Daily plan} */}
+            <PlacesToVisit trip={trip} />
+            {/* {footer} */}
+            <Footer />
+        </div>
+    )
 }
 export default Viewtrip
--- src/view-trip/components/Hotels.jsx
+++ src/view-trip/components/Hotels.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router'
 import HotelCardItem from './HotelCardItem'
 
 function Hotels({ trip }) {
@@ -8,7 +7,7 @@ function Hotels({ trip }) {
       <h2 className='font-bold text-xl mt-5'>Hotel Recommendation</h2>
       <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 mt-10 '>
         {trip?.tripData?.hotels?.map((hotels, index) => (
-          <HotelCardItem hotels={hotels}/>
+          <HotelCardItem hotels={hotels} key={hotels.id || index}/>
         ))}
       </div>
       
